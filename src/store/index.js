e877448cd0313f265c6f27cc8f9078c7214528ef
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-19 22:25:17 +0900

    ActionsでVuex使用
    API通信したresponseをstateに入れることで、
    毎回通信しないようにする。

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,39 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    globalCount: 5
+    globalCount: 0,
+    profile: null,
   },
   mutations: {
-    /* 第一引数には必ずstateが渡ってくる */
+    /* mutationsの第一引数には必ずstateが渡ってくる */
     globalIncrement(state) {
-      state.globalCount++
-    }
+      state.globalCount++;
+    },
+    setGithubProfile(state, payload) {
+      state.profile = payload;
+    },
   },
-  actions: {},
-  modules: {}
+  actions: {
+    /* 非同期でgithubAPIの情報取得 */
+    async fetchGithubProfile(store, payload) {
+
+      /*
+      stateの値がnullの場合だけ通信するようにする
+      これがないと毎回、リクエストすることになる
+      */
+      if (store.state.profile !== null) return;
+
+      const response = await fetch(
+        `https://api.github.com/users/${payload.user_id}`
+      ).then((res) => {
+        return res.json();
+      });
+
+      /*
+      返ってきた情報をstateに格納する為、mutationsのsetGithubProfileにcommitする
+      */
+      store.commit("setGithubProfile", response);
+    },
+  },
+  modules: {},
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>アバウト画面</h1>
+
+    <!-- このprofileはstoreのstateの変数 -->
+    <p>{{ profile }}</p>
   </div>
 </template>
+
+<script>
+/* storeのstateのprofileを使用したい為にimport */
+import { mapState } from "vuex";
+
+export default {
+  mounted() {
+    /* mountedのタイミングでactionsのfetchGithubProfileを実行 */
+    this.$store.dispatch("fetchGithubProfile", {
+      user_id: "octcat",
+    });
+  },
+  computed: {
+    /* storeのstateのprofileを使用できるようにする */
+    ...mapState(["profile"]),
+  },
+};
+</script>
