cdb5e4d568953c6a4366846de4c74a04cf10b0ff
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-19 17:39:05 +0900

    event upとprops down
    〜34P

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <Counter name="カウンター１" :initCount="5" />
+    <Counter name="カウンター１" :initCount="5" @emitUp="getEvent" />
+    <Counter name="カウンター２" :initCount="10" />
+    <Counter name="カウンター２" :initCount="10" @emitUp="getEvent" />
+    <p>
+      EventStack: {{ stack }}
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Counter from "@/components/Counter.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    /* importしてcomponentsに記述するとコンポーネントをローカル登録できる */
+    Counter,
+  },
+  data() {
+    return {
+      stack: [],
+    };
+  },
+  methods: {
+    getEvent(payload) {
+      this.stack.push(payload);
+    },
+  },
 };
 </script>
