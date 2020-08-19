5dcbf5963460982ad55fed57e2920fc4a4b35ca6
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-19 21:31:15 +0900

    ...mapStateでVuex使用
    その他、邪魔なの消す

--- src/views/Count.vue
+++ src/views/Count.vue
@@ -1,37 +1,16 @@
 <template>
   <div>
-    <Counter name="カウンター１" :initCount="5" @emitUp="getEvent" />
-    <Counter name="カウンター２" :initCount="10" @emitUp="getEvent" />
-
-    <p>computed経由でstateのglobalCountを表示：{{ count }}</p>
-    <p>primitiveStore：{{ primitiveStore.$data.globalCount }}</p>
-    <input type="text" v-model="primitiveStore.$data.globalCount" />
     <p>globalCount：{{ globalCount }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Counter from "@/components/Counter.vue";
-import primitiveStore from "@/primitiveStore.js";
 import { mapState } from "vuex";
 
 export default {
   name: "Home",
-  components: {
-    /* importしてcomponentsに記述するとコンポーネントをローカル登録できる */
-    Counter,
-  },
-  data() {
-    return {
-      primitiveStore,
-    };
-  },
   computed: {
-    count() {
-      /* this.$store.stateでstoreのstateにアクセスできる */
-      return this.$store.state.globalCount;
-    },
+    /* import後...mapState書いて、storeのstateのglobalCountを使用できる */
     ...mapState(["globalCount"]),
   },
   methods: {
